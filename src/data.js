ae5108a692f302d547ac2eaedf4b674c0b6262da
mpabarca <mpabarca@uc.cl>
2019-01-17 15:56:07 -0300

    data.js cambios por fetch + main.js primeras lineas de fetch

--- src/data.js
+++ src/data.js
@@ -1,17 +1,19 @@
 window.data ={
 
   filterData: (data,condition) => {
-    const lolData= data;
-    const championData= Object.values(lolData.data);
-    const category=championData.filter(tipos =>{
+    /*const lolData= data;
+    const championData= Object.values(lolData.data);*/
+    const championData=data;
+    const category=championData.filter(tipos=>{
     return tipos.tags.indexOf(condition)!==-1;
     });    
     return category;
   },
    
   sortData: (data,sortBy,sortOrder) => {
-    const lolData= data;
-    const championData= Object.values(lolData.data);
+    /*const lolData= data;
+    const championData= Object.values(lolData.data);*/
+    const championData=data;
     if(sortBy==="id" && sortOrder === "a-z" ){
       championData.sort((a,b)=>{
         if(b.id>a.id){
@@ -39,11 +41,11 @@ window.data ={
   
   findChampion: (data,id) => {
     const lolData = data
-    const championData= Object.values(lolData.data);
+    //const championData= Object.values(lolData.data);
     let championInfo;
-    for(let i=0; i<championData.length;i++){
-      if(championData[i].id===id){
-       championInfo=championData[i]
+    for(let i=0; i<lolData.length;i++){
+      if(lolData[i].id===id){
+       championInfo=lolData[i]
       }
     }
     return championInfo;
